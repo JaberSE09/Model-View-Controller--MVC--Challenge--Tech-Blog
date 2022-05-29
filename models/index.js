4875906903f61188fe5cfc5be4208d7aa8503d64
Sajid Jaber <sajidjaber@yahoo.com>
2022-05-29 12:06:01 -0500

    setup the routes

--- models/index.js
+++ models/index.js
@@ -1,5 +1,16 @@
 // import all models
 const Post = require('./post');
 const User = require('./user');
+//create associations
+User.hasMany(Post, {
+  foreignKey: 'user_id'
+});
+
+Post.belongsTo(User, {
+  foreignKey: 'user_id',
+});
+
+
+
 
 module.exports = { User, Post };
--- seeds/postData.js
+++ seeds/postData.js
@@ -4,15 +4,19 @@ const postData = [
   {
     title: "Donec posuere metus vitae ipsum.",
     desc: ",ll;kfdl;gkf;ll;",
+    user_id: 1,
   },
   {
     title: "Morbi non quam nec dui luctus rutrum.",
     dsc: "fdfdjkjk fdgjkfdjlkjgkljklkj jilkjlkdj;fd",
+    user_id: 1,
+
   },
   {
     title:
       "Donec diam neque, vestibulum eget, vulputate ut, ultrices vel, augue.",
     desc: "donec kdlal;dsakdsal;kldsakdl;k;l'kdsa'sadkldsads;lkdas' kdsaldsa;k;lddsa  ldsa;k dsal;dsa dsa",
+    user_id: 3,
   }
 ];
 
