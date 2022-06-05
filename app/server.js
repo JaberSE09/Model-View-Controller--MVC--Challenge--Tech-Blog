dd1c583d0529f96fbcef0d4230c7512740c51a62
Sajid Jaber <sajidjaber@yahoo.com>
2022-06-04 20:55:56 -0500

    heroku

--- app/server.js
+++ app/server.js
@@ -6,7 +6,7 @@ const exphbs = require('express-handlebars');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const sequelize = require("./config/connection");
+const sequelize = require("../config/connection");
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
@@ -25,7 +25,7 @@ const sess = {
 
 app.use(session(sess));
 
-const helpers = require('./utils/helpers');
+const helpers = require('../utils/helpers');
 
 const hbs = exphbs.create({ helpers });
 
@@ -36,7 +36,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(require('./controllers/'));
+app.use(require('../controllers'));
 
 
 sequelize.sync({ force: false }).then(() => {
